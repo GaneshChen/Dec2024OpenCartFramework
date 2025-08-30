4b8d66a403e82fcaae3ccfc397a224c1061f1ae2
Ganesh <gchenkalvarayan@ilinksolutions.com>
2025-08-29 21:10:57 -0400

    Updated

--- src/main/java/com/qa/opencart/pages/Util.java
+++ src/main/java/com/qa/opencart/pages/Util.java
@@ -5,4 +5,8 @@ public class Util {
     public void m1_local(){
         System.out.println("This is an m1_local method");
     }
+
+    public void m2_local(){
+        System.out.println("This is M2_Local");
+    }
 }
