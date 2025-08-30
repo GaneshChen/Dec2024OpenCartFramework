106726e48af5688c0258b8cd0067a06f37886cf9
Ganesh <gchenkalvarayan@ilinksolutions.com>
2025-08-30 10:25:36 -0400

    Updated Changes

--- src/main/java/com/qa/opencart/pages/MyUtil.java
+++ src/main/java/com/qa/opencart/pages/MyUtil.java
@@ -3,6 +3,9 @@ package com.qa.opencart.pages;
 public class MyUtil {
 
     public void login(){
+
         System.out.println("Remote -- Login");
+        System.out.println("Ganesh -- Login");
+
     }
 }
