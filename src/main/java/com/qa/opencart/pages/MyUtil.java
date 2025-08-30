323a164f9ff416045e4738c46bc48dd33be22de3
Ganesh <gchenkalvarayan@ilinksolutions.com>
2025-08-30 10:34:36 -0400

    Hello_world

--- src/main/java/com/qa/opencart/pages/MyUtil.java
+++ src/main/java/com/qa/opencart/pages/MyUtil.java
@@ -1,11 +1,14 @@
 package com.qa.opencart.pages;
 
+import software.amazon.awssdk.awscore.util.SignerOverrideUtils;
+
 public class MyUtil {
 
     public void login(){
 
         System.out.println("Remote -- Login");
         System.out.println("Ganesh -- Login");
+        
 
     }
 }
